f93cbb3394fb79415786b40bf116dfa2844d926d
barry-kn <carrybarryklein49@gmail.com>
2023-03-16 16:34:53 +0300

    sol commit

--- function.js
+++ function.js
@@ -1,6 +1,21 @@
-function sumNum(a,b,c,d)
+/*function sumNum(a,b,c,d)
 {
     var total =(a+b+c+d);
+    
     document.write("<h2>This is my ans: "+total+"</h2>");
 }
-sumNum(3,4,6,7)
+sumNum(3,4,6,7) */
+   
+
+//solutio
+
+function getArraySum (arr) {
+    let sum = 0; //initialize 
+    for (let i=0; i<arr.length; i++){
+        sum += arr[i];
+    }
+    return sum;  
+}
+let arr1 = [1,2,3,4];
+
+console.log(getArraySum(arr1));
