2bb68c4f7699642831b8817f3b46cd3e8ab1287f
barry-kn <carrybarryklein49@gmail.com>
2023-03-16 11:06:07 +0300

    solution commit

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,25 @@
-function tryPalindrome(c){
-    if (c==="palindrome"){
-      return result="TRUE."
-    }
-    else{
-      return result="FALSE."
-    }
-    }
-    tryPalindrome()
+//function tryPalindrome(c){
+  //  if (c==="palindrome"){
+//      return result="TRUE."
+//    }
+ //   else{
+ //     return result="FALSE."
+  //  }
+//    }
+//    tryPalindrome()
+
+//solution
+
+function isPalindrome(testString){
+  let reversedString = testString.split('').reverse().join('');
+  if (testString === reversedString){
+      return true;
+  }else{
+       return false
+  }
+
+  
+}
+
+console.log(isPalindrome("121"));
+console.log(isPalindrome("john"));
